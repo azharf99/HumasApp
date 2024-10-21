5f92841be96a318b39e43dab75151fd8893c1ebf
azharfa99 <azharfaturohman29@gmail.com>
2024-10-21 09:35:35 +0700

    changing Whatsapp sender

--- utils/whatsapp.py
+++ utils/whatsapp.py
@@ -8,7 +8,8 @@ def send_WA_general(phone="085701570100", action="", messages=""):
 Anda berhasil *{action}* *{messages}*.
 
 _Ini adalah pesan otomatis, jangan dibalas._'''
-    url = f"https://jogja.wablas.com/api/send-message?phone={phone}&message={message}&token={token}"
+    # url = f"https://jogja.wablas.com/api/send-message?phone={phone}&message={message}&token={token}"
+    url = f"https://albinaa.sch.id/wp-content/wa/api.php?sender=6285157030478&no=62{phone[1:] if phone is not '0' else '85701570100'}&pesan={message}"
     try:
         data = requests.get(url)
         return data
@@ -23,7 +24,9 @@ Jika ada yang ditanyakan terkait aplikasi, silahkan hubungi:
 https://wa.me/6285701570100
 
 _Ini adalah pesan otomatis, jangan dibalas._'''
-    url = f"https://jogja.wablas.com/api/send-message?phone={phone}&message={message}&token={token}"
+    # url = f"https://jogja.wablas.com/api/send-message?phone={phone}&message={message}&token={token}"
+    url = f"https://albinaa.sch.id/wp-content/wa/api.php?sender=6285157030478&no=62{phone[1:] if phone is not '0' else '85701570100'}&pesan={message}"
+
     try:
         data = requests.get(url)
         return data
@@ -68,7 +71,9 @@ Detail laporan:
 https://humas.smasitalbinaa.com/{type}{slug}
 
 _Ini adalah pesan otomatis, jangan dibalas._'''
-    url = f"https://jogja.wablas.com/api/send-message?phone={phone}&message={message}&token={token}"
+    # url = f"https://jogja.wablas.com/api/send-message?phone={phone}&message={message}&token={token}"
+    url = f"https://albinaa.sch.id/wp-content/wa/api.php?sender=6285157030478&no=62{phone[1:] if phone is not '0' else '85701570100'}&pesan={message}"
+
     try:
         data = requests.get(url)
         return data
@@ -81,7 +86,9 @@ def send_WA_print(phone="085701570100", doc_type="", messages=""):
 Anda berhasil mencetak {doc_type} {messages}.
 
 _Ini adalah pesan otomatis, jangan dibalas._'''
-    url = f"https://jogja.wablas.com/api/send-message?phone={phone}&message={message}&token={token}"
+    # url = f"https://jogja.wablas.com/api/send-message?phone={phone}&message={message}&token={token}"
+    url = f"https://albinaa.sch.id/wp-content/wa/api.php?sender=6285157030478&no=62{phone[1:] if phone is not '0' else '85701570100'}&pesan={message}"
+
     try:
         data = requests.get(url)
         return data
